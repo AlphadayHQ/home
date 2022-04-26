d9614cb8ba873628f6d116f1a277984b2e5500a0
Xharles <charlesnzubechukwu000@gmail.com>
2022-04-26 07:47:25 +0100

    other styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,31 +13,36 @@ module.exports = {
       blue: "var(--color-palatinate-blue)",
       eerie: "var(--color-eerie-black)",
       white: "var(--color-white)",
-      black: "var(--color-black)"
+      black: "var(--color-black)",
     },
 
     extend: {
       dropShadow: {
-        "eclipse": "4px 4px 0px #000000"
+        eclipse: "4px 4px 0px #000000",
       },
       keyframes: {
         textFade: {
           "0% ": { marginTop: "-360px" },
-          "5%" : { marginTop: "-270px" },
-          "25%" : { marginTop: "-270px" },
-          "30%" : { marginTop: "-180px" },
-          "50%" : { marginTop: "180px" },
-          "55%" : { marginTop: "-90px" },
-          "75%" : { marginTop: "-90px" },
-          "80%" : { marginTop: "0px" },
-          "99.99%" : { marginTop: "0px" },
-          "100%" : { marginTop: "-270px" }
-        }
+          "5%": { marginTop: "-270px" },
+          "25%": { marginTop: "-270px" },
+          "30%": { marginTop: "-180px" },
+          "50%": { marginTop: "-180px" },
+          "55%": { marginTop: "-90px" },
+          "75%": { marginTop: "-90px" },
+          "80%": { marginTop: "0px" },
+          "99.9%": { marginTop: "0px" },
+          "100%": { marginTop: "-270px" },
+        },
       },
       animation: {
+
         'textFade': "textFade 10s cubic-bezier(0.23, 1, 0.32, 1.2) infinite"
+      },
+      fontFamily: {
+        titling: ["titling", defaultTheme.fontFamily.sans],
+        sans: ["titling", defaultTheme.fontFamily.sans]
       }
     }
   },
   plugins: [],
-}
+};
