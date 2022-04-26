838fd4b848a9d0aadf2b3b6a1ed7a2d4e42aa72e
Xharles <charlesnzubechukwu000@gmail.com>
2022-04-26 08:52:49 +0100

    handle images

--- src/components/home/Team.jsx
+++ src/components/home/Team.jsx
@@ -1,12 +1,87 @@
-import React from "react"
+import React from "react";
 import { Section, Div, Title } from "../../shared";
-import member1 from "../../images/team/member-1.png";
-import member2 from "../../images/team/member-2.png";
-import member3 from "../../images/team/member-3.png";
-import member4 from "../../images/team/member-4.png";
-import member5 from "../../images/team/member-5.png";
-import member6 from "../../images/team/member-6.png";
-import member7 from "../../images/team/member-7.png";
+import member1 from "../../images/team/member-1.jpg";
+import member2 from "../../images/team/member-2.jpg";
+import member3 from "../../images/team/member-3.jpg";
+import member4 from "../../images/team/member-4.jpg";
+import member5 from "../../images/team/member-5.jpg";
+import member6 from "../../images/team/member-6.jpg";
+import member7 from "../../images/team/member-7.jpg";
+
+const teamData = [
+  {
+    img: member1,
+    classnames: "rounded-[26px] w-[137px] h-[137px]",
+    name: "Gideon Anyalewechi",
+    position: "CO-FOUNDER & CMO",
+  },
+  {
+    img: member2,
+    classnames: "rounded-[26px] w-[158px] h-[158px]",
+    name: "DeFi Dude",
+    position: "CO-FOUNDER & CMO",
+  },
+  {
+    img: member3,
+    classnames: "rounded-[26px] w-[183px] h-[183px]",
+    name: "Felipe Faraggi",
+    position: "CO-FOUNDER & CMO",
+  },
+  {
+    img: member4,
+    classnames: "rounded-[26px] w-[151px] h-[151px]",
+    name: "Vicente Almonacid",
+    position: "CO-FOUNDER & CMO",
+  },
+  {
+    img: member5,
+    classnames: "rounded-[26px] w-[196px] h-[196px]",
+    name: "Pablo Palomo",
+    position: "CO-FOUNDER & CMO",
+  },
+  {
+    img: member6,
+    classnames: "rounded-[26px] w-[222px] h-[222px]",
+    name: "Deniz Omar",
+    position: "CO-FOUNDER & CMO",
+  },
+  {
+    img: member7,
+    classnames: "rounded-[26px] w-[151px] h-[151px]",
+    name: "Charles Nwankwo",
+    position: "CO-FOUNDER & CMO",
+  },
+];
+
+function TeamCard({ data, type }) {
+  return (
+    <div
+      className={`${type === "bottom" ? "self-start mt-5" : "self-end"} group`}
+    >
+      <figure className="relative flex justify-center items-center">
+        <span className="absolute w-full h-full bg-[#1D1F2090] group-hover:bg-transparent transition-all duration-300"></span>
+        <img src={data.img} className={data.classnames} alt={data.name} />
+
+        <div className="scale-0 group-hover:scale-100 transition-all duration-300 rounded-xl absolute z-20 text-white py-2 px-4 bg-black min-w-max left-[70%]">
+          <div className="mb-4">
+            <h4 className="text-[16px]">{data.name}</h4>
+            <p className="text-[10px] tracking-[.2em]">{data.position}</p>
+          </div>
+
+          <div className="flex">
+            <a className="cursor-pointer flex items-center w-[22px] h-[22px] rounded-full justify-center bg-blue mr-2">
+              <i className="text-white text-xs ri-twitter-fill"></i>
+            </a>
+
+            <a className="cursor-pointer flex items-center w-[22px] h-[22px] rounded-full justify-center bg-blue">
+              <i class="text-white text-xs ri-linkedin-fill"></i>
+            </a>
+          </div>
+        </div>
+      </figure>
+    </div>
+  );
+}
 
 function Team() {
   return (
@@ -34,25 +109,28 @@ function Team() {
 
           <div className="flex justify-center items-center relative">
             <p
-              className="absolute z-10 top-[50%] left-[50%] text-aluminium text-xl"
+              className="absolute z-10 top-[50%] left-[50%] font-medium text-aluminium text-[22px] tracking-[1px]"
               style={{ transform: "translate(-50%, -50%)" }}
             >
               OUR TEAM
             </p>
             <div className="">
-
               <div className="flex gap-x-2">
-                
-                <div className="self-end group">
+                {teamData.slice(0, 4).map((data) => (
+                  <TeamCard data={data} type="top" />
+                ))}
+                {/* <div className="self-end group">
                   <figure className="relative flex justify-center items-center">
                     <span className="absolute w-full h-full bg-[#1D1F2090] group-hover:bg-transparent transition-all duration-300"></span>
-                    <img src={member1} alt="member1"/>
+                    <img src={member1} alt="member1" />
                   </figure>
 
                   <div className="scale-0 group-hover:scale-100 transition-all duration-300 rounded-xl absolute z-20 top-[70px] left-0 md:top-[50px] lg:left-[150px] lg:top-[150px] xl:top-[100px] xl:left-[350px] text-white py-2 px-4 bg-black">
                     <div className="mb-4">
                       <h4 className="text-[16px]">DeFi Dude</h4>
-                      <p className="text-[10px] tracking-[.2em]">CO-FOUNDER & CMO</p>
+                      <p className="text-[10px] tracking-[.2em]">
+                        CO-FOUNDER & CMO
+                      </p>
                     </div>
 
                     <div className="flex">
@@ -70,13 +148,15 @@ function Team() {
                 <div className="self-end group">
                   <figure className="relative flex justify-center items-center">
                     <span className="absolute w-full h-full bg-[#1D1F2090] group-hover:bg-transparent transition-all duration-300"></span>
-                    <img src={member2} alt="member2"/>
+                    <img src={member2} alt="member2" />
                   </figure>
 
                   <div className="scale-0 group-hover:scale-100 transition-all duration-300 rounded-xl absolute z-20 top-[70px] left-[20px] md:top-[100px] md:left-[150px] lg:left-[350px] xl:top-[90px] xl:left-[500px] text-white py-2 px-4 bg-black">
                     <div className="mb-4">
                       <h4 className="text-[16px]">DeFi Dude</h4>
-                      <p className="text-[10px] tracking-[.2em]">CO-FOUNDER & CMO</p>
+                      <p className="text-[10px] tracking-[.2em]">
+                        CO-FOUNDER & CMO
+                      </p>
                     </div>
 
                     <div className="flex">
@@ -94,13 +174,15 @@ function Team() {
                 <div className="mb-4 group">
                   <figure className="relative flex justify-center items-center">
                     <span className="absolute w-full h-full bg-[#1D1F2090] group-hover:bg-transparent transition-all duration-300"></span>
-                    <img src={member3} alt="member3"/>
+                    <img src={member3} alt="member3" />
                   </figure>
 
                   <div className="scale-0 group-hover:scale-100 transition-all duration-300 rounded-xl absolute z-20 top-[50px] left-[70px] md:left-[400px] md:top-[80px] lg:left-[500px] xl:top-[60px] xl:left-[700px] text-white py-2 px-4 bg-black">
                     <div className="mb-4">
                       <h4 className="text-[16px]">DeFi Dude</h4>
-                      <p className="text-[10px] tracking-[.2em]">CO-FOUNDER & CMO</p>
+                      <p className="text-[10px] tracking-[.2em]">
+                        CO-FOUNDER & CMO
+                      </p>
                     </div>
 
                     <div className="flex">
@@ -118,13 +200,15 @@ function Team() {
                 <div className="self-end mb-4 group">
                   <figure className="relative flex justify-center items-center">
                     <span className="absolute w-full h-full bg-[#1D1F2090] group-hover:bg-transparent transition-all duration-300"></span>
-                    <img src={member4} alt="member4"/>
+                    <img src={member4} alt="member4" />
                   </figure>
 
                   <div className="scale-0 group-hover:scale-100 transition-all duration-300 rounded-xl absolute z-20 top-[70px] left-[180px] md:left-[500px] md:top-[130px] lg:left-[600px] xl:top-[120px] xl:left-[750px] text-white py-2 px-4 bg-black">
                     <div className="mb-4">
                       <h4 className="text-[16px]">DeFi Dude</h4>
-                      <p className="text-[10px] tracking-[.2em]">CO-FOUNDER & CMO</p>
+                      <p className="text-[10px] tracking-[.2em]">
+                        CO-FOUNDER & CMO
+                      </p>
                     </div>
 
                     <div className="flex">
@@ -137,20 +221,25 @@ function Team() {
                       </a>
                     </div>
                   </div>
-                </div>
+                </div> */}
               </div>
-              
+
               <div className="flex gap-x-2 justify-end items-center">
-                <div className="group">
+                {teamData.slice(4, 7).map((data) => (
+                  <TeamCard data={data} type="bottom" />
+                ))}
+                {/* <div className="group">
                   <figure className="relative flex justify-center items-center">
                     <span className="absolute w-full h-full bg-[#1D1F2090] group-hover:bg-transparent transition-all duration-300"></span>
-                    <img src={member5} alt="member5"/>
+                    <img src={member5} alt="member5" />
                   </figure>
 
                   <div className="scale-0 group-hover:scale-100 transition-all duration-300 rounded-xl absolute z-20 bottom-[-35px] left-[20px] md:left-[70px] md:bottom-[0px] lg:left-[200px] xl:bottom-[30px] xl:left-[300px] text-white py-2 px-4 bg-black">
                     <div className="mb-4">
                       <h4 className="text-[16px]">DeFi Dude</h4>
-                      <p className="text-[10px] tracking-[.2em]">CO-FOUNDER & CMO</p>
+                      <p className="text-[10px] tracking-[.2em]">
+                        CO-FOUNDER & CMO
+                      </p>
                     </div>
 
                     <div className="flex">
@@ -168,13 +257,15 @@ function Team() {
                 <div className="group">
                   <figure className="relative flex justify-center items-center">
                     <span className="absolute w-full h-full bg-[#1D1F2090] group-hover:bg-transparent transition-all duration-300"></span>
-                    <img src={member6} alt="member6"/>
+                    <img src={member6} alt="member6" />
                   </figure>
 
                   <div className="scale-0 group-hover:scale-100 transition-all duration-300 rounded-xl absolute z-20 left-[50px] bottom-[-35px] md:left-[300px] md:bottom-[0px] lg:left-[400px] xl:bottom-[0px] xl:left-[550px] text-white py-2 px-4 bg-black">
                     <div className="mb-4">
                       <h4 className="text-[16px]">DeFi Dude</h4>
-                      <p className="text-[10px] tracking-[.2em]">CO-FOUNDER & CMO</p>
+                      <p className="text-[10px] tracking-[.2em]">
+                        CO-FOUNDER & CMO
+                      </p>
                     </div>
 
                     <div className="flex">
@@ -192,13 +283,15 @@ function Team() {
                 <div className="self-start group">
                   <figure className="relative flex justify-center items-center">
                     <span className="absolute w-full h-full bg-[#1D1F2090] group-hover:bg-transparent transition-all duration-300"></span>
-                    <img src={member7} alt="member7"/>
+                    <img src={member7} alt="member7" />
                   </figure>
 
                   <div className="scale-0 group-hover:scale-100 transition-all duration-300 rounded-xl absolute z-20 left-[150px] bottom-[-25px] md:left-[500px] md:bottom-[20px] lg:left-[600px] xl:bottom-[30px] xl:left-[700px] text-white py-2 px-4 bg-black">
                     <div className="mb-4">
                       <h4 className="text-[16px]">DeFi Dude</h4>
-                      <p className="text-[10px] tracking-[.2em]">CO-FOUNDER & CMO</p>
+                      <p className="text-[10px] tracking-[.2em]">
+                        CO-FOUNDER & CMO
+                      </p>
                     </div>
 
                     <div className="flex">
@@ -211,9 +304,8 @@ function Team() {
                       </a>
                     </div>
                   </div>
-                </div>
+                </div> */}
               </div>
-              
             </div>
           </div>
         </div>
@@ -223,5 +315,3 @@ function Team() {
 }
 
 export default Team;
-
-
