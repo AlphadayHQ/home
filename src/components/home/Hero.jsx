003579a71bb9895ad3941a7af78ada0729afcea7
Xharles <charlesnzubechukwu000@gmail.com>
2022-04-27 12:40:19 +0100

    add animation

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Div, Section } from "../../shared";
+import { Button, Section } from "../../shared";
 import hero from "../../images/home/hero.png";
 import "./textScroller.css";
 
@@ -16,26 +16,24 @@ const Dashboard = () => {
 };
 
 export default function () {
+  const textOptions = ["NFT", "DeFi", "Crypto", "Ethereum"];
   return (
     <Section className="bg-california overflow-hidden h-auto w-full border-t border-california">
       <div className="mx-auto w-11/12 max-w-7xl">
         <div className=" md:mx-auto max-w-5xl pt-16 md:pt-16 mt-14">
-          <div>
-            <h1 className="text-black text-center m-0 md:text-left font-medium text-[38px] md:text-7xl lg:text-[98px]">
-              Everything NFT.
-              {/* <div className="scroller">
-                            <span>
-                                NFT. <br/>
-                                DeFi. <br/>
-                                Crypto. <br/>
-                                Ethereum.
-                            </span>
-                        </div> */}
-            </h1>
-            <h1 className="text-black text-[38px] font-medium  m-0 text-center md:text-left md:text-7xl lg:text-[100px]">
-              All in one place.
-            </h1>
-          </div>
+          <h1 class="flex text-black text-center m-0 md:text-left font-medium leading-[110px] text-[38px] md:text-7xl lg:text-[98px]">
+            <span>Everything</span>
+            <div className="overflow-hidden relative h-[125px] pt-2.5 -mt-2.5 flex">
+              <ul class="flip4 max-w-[500px] inline-block">
+                {textOptions.map((text) => (
+                  <li className="pl-6 h-[125px] mb-[15px] block">{text}.</li>
+                ))}
+              </ul>
+            </div>
+          </h1>
+          <h1 className="text-black text-[38px] font-medium  m-0 text-center md:text-left md:text-7xl lg:text-[100px]">
+            All in one place.
+          </h1>
 
           <div className="w-full md:w-[600px] flex flex-col md:flex-row justify-between items-center mt-4 md:mt-8">
             <p className="w-full text-center text-sm md:w-[430px] md:text-lg md:text-left text-[#00000090]">
