6fe459dfb69458945ed23e3027b99f82c01c3aad
Emmanuel James <76987753+DAVIS-PYTH@users.noreply.github.com>
2022-05-17 21:18:40 +0100

    home01: fixed team members name (#2)
    * fixed team members name
    
    * updated the twitter link of team member

--- src/components/home/Team.jsx
+++ src/components/home/Team.jsx
@@ -27,9 +27,9 @@ const teamData = [
     classnames: "rounded-[13px] sm:rounded-[26px] w-[63px]  sm:w-[158px]",
     infoPosClassnames:
       "bottom-[-110%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
-    name: "https://twitter.com/defidude",
+    name: "Defi Dude",
     position: "CO-FOUNDER & CMO",
-    twitter: "",
+    twitter: "https://twitter.com/defidude",
     linkedin: "https://www.linkedin.com/in/defi-dude-72296b221/",
   },
   {
