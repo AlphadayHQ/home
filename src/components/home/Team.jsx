223e387f597b8f04b8308421823b15baa72420f1
Xharles <charlesnzubechukwu000@gmail.com>
2022-06-02 10:07:01 +0100

    randomise the order of  team members

--- src/components/home/Team.jsx
+++ src/components/home/Team.jsx
@@ -1,128 +1,6 @@
 import React from "react";
 import { Section, Div, Title } from "../../shared";
-import member1 from "../../images/team/member-1.jpg";
-import member2 from "../../images/team/member-2.jpg";
-import member3 from "../../images/team/member-3.jpg";
-import member4 from "../../images/team/member-4.jpg";
-import member5 from "../../images/team/member-5.jpg";
-import member6 from "../../images/team/member-6.jpg";
-import member7 from "../../images/team/member-7.jpg";
-import member8 from "../../images/team/member-8.jpg";
-import member9 from "../../images/team/member-9.jpg";
-import member10 from "../../images/team/member-10.jpg";
-
-const teamData = [
-  {
-    id: 1,
-    img: member1,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[63px]  sm:w-[158px]",
-    infoPosClassnames:
-      "left-[30%] bottom-[-140%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
-    name: "Gideon Anyalewechi",
-    position: "Developer",
-    twitter: "https://twitter.com/get_giddy",
-    linkedin: "https://www.linkedin.com/in/getgiddy/",
-  },
-  {
-    id: 2,
-    img: member2,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[60px] sm:w-[151px]",
-    infoPosClassnames:
-      "bottom-[-110%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
-    name: "Defi Dude",
-    position: "Co-Founder",
-    twitter: "https://twitter.com/defidude",
-    linkedin: "https://www.linkedin.com/in/defi-dude-72296b221/",
-  },
-  {
-    id: 3,
-    img: member3,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[73px] sm:w-[183px]",
-    infoPosClassnames:
-      "bottom-[-100%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
-    name: "Felipe Faraggi",
-    position: "Co-Founder & CTO",
-    twitter: "https://twitter.com/felipefaraggi",
-    linkedin: "https://www.linkedin.com/in/faraggi/",
-  },
-  {
-    id: 4,
-    img: member4,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[60px] sm:w-[151px]",
-    infoPosClassnames:
-      "bottom-[-120%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
-    name: "Vicente Almonacid",
-    position: "Software Engineer",
-    twitter: "https://twitter.com/v_almonacid",
-    linkedin: "https://www.linkedin.com/in/vicente-almonacid/",
-  },
-  {
-    id: 5,
-    img: member5,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[63px]  sm:w-[158px]",
-    infoPosClassnames:
-      "right-[30%] bottom-[-140%] sm:bottom-[-15%] lg:bottom-auto lg:left-[70%]",
-    name: "Pablo Palomo",
-    position: "Technical Lead",
-    twitter: "https://twitter.com/ppalomo",
-    linkedin: "https://www.linkedin.com/in/pablo-palomo-07127711/",
-  },
-  {
-    id: 6,
-    img: member9,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[63px]  sm:w-[158px]",
-    infoPosClassnames:
-      "left-[30%] bottom-[-100%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
-    name: "Jonathan Irhodia",
-    position: "Developer",
-    twitter: "https://twitter.com/iamelcharitas",
-    linkedin: "https://linkedin.com/in/elcharitas",
-  },
-  {
-    id: 7,
-    img: member10,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[60px] sm:w-[151px]",
-    infoPosClassnames:
-      "bottom-[-120%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
-    name: "Mikael Hagopian",
-    position: "QA and Analyst",
-    twitter: "https://twitter.com/MikeJa33",
-    linkedin: "https://www.linkedin.com/in/mikael-h-87bb4ba4/",
-  },
-  {
-    id: 9,
-    img: member6,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[73px] sm:w-[183px]",
-    infoPosClassnames:
-      "bottom-[-80%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
-    name: "Deniz Omar",
-    position: "Co-Founder & CEO",
-    twitter: "https://twitter.com/DenizOmer",
-    linkedin: "https://www.linkedin.com/in/denizomer/",
-  },
-  {
-    id: 10,
-    img: member7,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[60px] sm:w-[151px]",
-    infoPosClassnames:
-      "bottom-[-120%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
-    name: "Charles Nwankwo",
-    position: "Developer",
-    twitter: "https://twitter.com/Chadnium",
-    linkedin: "https://www.linkedin.com/in/getgiddy/",
-  },
-  {
-    id: 11,
-    img: member8,
-    classnames: "rounded-[13px] sm:rounded-[26px] w-[63px]  sm:w-[158px]",
-    infoPosClassnames:
-      "right-[30%] bottom-[-140%] sm:bottom-[-15%] lg:bottom-auto lg:left-[70%]",
-    name: "Paris Charanas Giannakou",
-    position: "Analyst and Content Editor",
-    twitter: "https://twitter.com/GrifousG",
-    linkedin: "https://www.linkedin.com/in/paris-charanas-giannakou-788386231/",
-  },
-];
+import { positionData, shuffleTeam, teamData } from "./teamUtils";
 
 function TeamCard({ data, type }) {
   return (
@@ -165,12 +43,12 @@ function TeamCard({ data, type }) {
 }
 
 function Team() {
+  const shuffledTeam = shuffleTeam(teamData, positionData);
   return (
     <Section>
       <Div>
         <div className="">
           <div className="mb-24 w-full flex flex-col items-center">
-            {/* <div className="w-[300px]"> */}
             <Title>Our Mission</Title>
             <div className="md:w-[500px]">
               <p className="text-aluminium text-xs md:text-base text-center">
@@ -186,7 +64,6 @@ function Team() {
                 narratives, and use your favourite dapps, all from the comfort
                 of one easy-to-use customizable dashboard.
               </p>
-              {/* </div> */}
             </div>
           </div>
 
@@ -199,13 +76,13 @@ function Team() {
             </p>
             <div className="flex flex-col">
               <div className="flex gap-x-2 justify-center">
-                {teamData.slice(0, 5).map((data) => (
+                {shuffledTeam.slice(0, 5).map((data) => (
                   <TeamCard key={data.id} data={data} type="top" />
                 ))}
               </div>
 
               <div className="flex mt-1.5 sm:mt-2 gap-x-2 justify-center items-center">
-                {teamData.slice(5, 11).map((data) => (
+                {shuffledTeam.slice(5, 11).map((data) => (
                   <TeamCard key={data.id} data={data} type="bottom" />
                 ))}
               </div>
---
+++ src/components/home/teamUtils.js
@@ -0,0 +1,170 @@
+import member1 from "../../images/team/member-1.jpg";
+import member2 from "../../images/team/member-2.jpg";
+import member3 from "../../images/team/member-3.jpg";
+import member4 from "../../images/team/member-4.jpg";
+import member5 from "../../images/team/member-5.jpg";
+import member6 from "../../images/team/member-6.jpg";
+import member7 from "../../images/team/member-7.jpg";
+import member8 from "../../images/team/member-8.jpg";
+import member9 from "../../images/team/member-9.jpg";
+import member10 from "../../images/team/member-10.jpg";
+
+/**
+ * For every team member card the the info tooltip is 
+ * positioned differently for mobile (you don't want the tooltip 
+ * to go out of the screen especially cards on the edged). 
+ * 
+ * So the position & size styles are constant while the team members
+ * data are shuffled.
+ */ 
+export const teamData = [
+  {
+    img: member1,
+    name: "Gideon Anyalewechi",
+    position: "Developer",
+    twitter: "https://twitter.com/get_giddy",
+    linkedin: "https://www.linkedin.com/in/getgiddy/",
+  },
+  {
+    img: member2,
+    name: "Defi Dude",
+    position: "Co-Founder",
+    twitter: "https://twitter.com/defidude",
+    linkedin: "https://www.linkedin.com/in/defi-dude-72296b221/",
+  },
+  {
+    img: member3,
+    name: "Felipe Faraggi",
+    position: "Co-Founder & CTO",
+    twitter: "https://twitter.com/felipefaraggi",
+    linkedin: "https://www.linkedin.com/in/faraggi/",
+  },
+  {
+    img: member4,
+    name: "Vicente Almonacid",
+    position: "Software Engineer",
+    twitter: "https://twitter.com/v_almonacid",
+    linkedin: "https://www.linkedin.com/in/vicente-almonacid/",
+  },
+  {
+    img: member5,
+    name: "Pablo Palomo",
+    position: "Technical Lead",
+    twitter: "https://twitter.com/ppalomo",
+    linkedin: "https://www.linkedin.com/in/pablo-palomo-07127711/",
+  },
+  {
+    img: member9,
+    name: "Jonathan Irhodia",
+    position: "Developer",
+    twitter: "https://twitter.com/iamelcharitas",
+    linkedin: "https://linkedin.com/in/elcharitas",
+  },
+  {
+    img: member10,
+    name: "Mikael Hagopian",
+    position: "QA and Analyst",
+    twitter: "https://twitter.com/MikeJa33",
+    linkedin: "https://www.linkedin.com/in/mikael-h-87bb4ba4/",
+  },
+  {
+    img: member6,
+    name: "Deniz Omar",
+    position: "Co-Founder & CEO",
+    twitter: "https://twitter.com/DenizOmer",
+    linkedin: "https://www.linkedin.com/in/denizomer/",
+  },
+  {
+    img: member7,
+    name: "Charles Nwankwo",
+    position: "Developer",
+    twitter: "https://twitter.com/Chadnium",
+    linkedin: "https://www.linkedin.com/in/getgiddy/",
+  },
+  {
+    img: member8,
+    name: "Paris Charanas Giannakou",
+    position: "Analyst and Content Editor",
+    twitter: "https://twitter.com/GrifousG",
+    linkedin: "https://www.linkedin.com/in/paris-charanas-giannakou-788386231/",
+  },
+];
+
+export const positionData = [
+  {
+    id: 1,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[63px]  sm:w-[158px]",
+    infoPosClassnames:
+      "left-[30%] bottom-[-140%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
+  },
+  {
+    id: 2,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[60px] sm:w-[151px]",
+    infoPosClassnames:
+      "bottom-[-110%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
+  },
+  {
+    id: 3,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[73px] sm:w-[183px]",
+    infoPosClassnames:
+      "bottom-[-100%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
+  },
+  {
+    id: 4,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[60px] sm:w-[151px]",
+    infoPosClassnames:
+      "bottom-[-120%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
+  },
+  {
+    id: 5,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[63px]  sm:w-[158px]",
+    infoPosClassnames:
+      "right-[30%] bottom-[-140%] sm:bottom-[-15%] lg:bottom-auto lg:left-[70%]",
+  },
+  {
+    id: 6,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[63px]  sm:w-[158px]",
+    infoPosClassnames:
+      "left-[30%] bottom-[-100%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
+  },
+  {
+    id: 7,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[60px] sm:w-[151px]",
+    infoPosClassnames:
+      "bottom-[-120%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
+  },
+  {
+    id: 8,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[73px] sm:w-[183px]",
+    infoPosClassnames:
+      "bottom-[-80%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
+  },
+  {
+    id: 9,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[60px] sm:w-[151px]",
+    infoPosClassnames:
+      "bottom-[-120%] sm:bottom-[-35%] lg:bottom-auto lg:left-[70%]",
+  },
+  {
+    id: 10,
+    classnames: "rounded-[13px] sm:rounded-[26px] w-[63px]  sm:w-[158px]",
+    infoPosClassnames:
+      "right-[30%] bottom-[-140%] sm:bottom-[-15%] lg:bottom-auto lg:left-[70%]",
+  },
+];
+
+export const shuffleArray = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]]; // The shuffle
+  }
+  return a;
+};
+
+export const shuffleTeam = (teamData, positionData) => {
+  if (teamData?.length === positionData?.length) {
+    const shuffledMembers = shuffleArray(teamData);
+    return positionData.map((data, i) => ({ ...data, ...shuffledMembers[i] }));
+  }
+};
+
