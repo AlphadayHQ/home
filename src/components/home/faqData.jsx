969eb314cfbd61c70173094da76c80f522220d2c
Charles Nwankwo <39612820+Xavier-Charles@users.noreply.github.com>
2022-06-14 13:50:30 +0100

    update faq data (#31)

--- src/components/home/faqData.jsx
+++ src/components/home/faqData.jsx
@@ -1,48 +1,61 @@
-const data =[
-    {
-        id: "1",
-        question: "What is Alphaday?",
-        answer: [
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>,
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        ]
-    },
+const data = [
+  {
+    id: "1",
+    question: "What is Alphaday?",
+    answer: [
+      <p>
+        Alphaday is the ultimate crypto dashboard for staying up to date with
+        all things crypto and interacting with your favorite DApps. It is built
+        on a highly customizable platform that lets you re-create your entire
+        crypto workflow within minutes so you never have to visit another crypto
+        website ever again.
+      </p>,
+    ],
+  },
 
-    {
-        id: "2",
-        question: "How much does Alphaday cost?",
-        answer: [
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>,
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        ]
-    },
+  {
+    id: "2",
+    question: "How much does Alphaday cost?",
+    answer: [<p>Alphaday is completely free to use.</p>],
+  },
 
-    {
-        id: "3",
-        question: "Lorem ipsum dolor sit amet elit?",
-        answer: [
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>,
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        ]
-    },
+  {
+    id: "3",
+    question: "Do I need to be an expert to use Alphaday?",
+    answer: [
+      <p>
+        No, Alphaday is built from the ground up to be beginner friendly. A
+        dedicated Beginner View provides a curated experience to ease users into
+        the world of crypto.
+      </p>,
+    ],
+  },
 
-    {
-        id: "4",
-        question: "Ut enim ad minim veniam nisi?",
-        answer: [
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>,
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        ]
-    },
+  {
+    id: "4",
+    question: "How secure is Alphaday?",
+    answer: [
+      <p>Alphaday is not a DApp and does not take custody of your tokens.</p>,
+    ],
+  },
 
-    {
-        id: "5",
-        question: "sunt in culpa qui officia laborum?",
-        answer: [
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>,
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        ]
-    }
-]
+//   {
+//     id: "5",
+//     question: "sunt in culpa qui officia laborum?",
+//     answer: [
+//       <p>
+//         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+//         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+//         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+//         commodo consequat.
+//       </p>,
+//       <p>
+//         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+//         dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+//         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+//       </p>,
+//     ],
+//   },
+];
 
 export { data };
