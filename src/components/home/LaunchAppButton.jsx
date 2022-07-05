01871a066d95beb9d5bc2ff795a2f0814551d7c9
Xharles <charlesnzubechukwu000@gmail.com>
2022-07-05 19:52:02 +0100

    disabled fix

--- src/components/home/LaunchAppButton.jsx
+++ src/components/home/LaunchAppButton.jsx
@@ -9,8 +9,7 @@ const LaunchAppButton = () => {
       </Button> */}
       <Button
         disabled
-        link={"https://app.alphaday.com"}
-        className="mt-4 md:mt-0 bg-[#585858] self-start md:self-center"
+        className="mt-4 md:mt-0 bg-[#585858] hover:opacity-100 cursor-not-allowed self-start md:self-center"
       >
         Coming soon
       </Button>
