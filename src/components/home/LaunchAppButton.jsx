485ebdcc2e64c9649576a8ef2febbba9e6e11be7
faraggi <{ID}+{username}@users.noreply.github.com>
2022-08-03 16:58:27 +0200

    edited new launch button
    Signed-off-by: faraggi <{ID}+{username}@users.noreply.github.com>

--- src/components/home/LaunchAppButton.jsx
+++ src/components/home/LaunchAppButton.jsx
@@ -7,14 +7,8 @@ const LaunchAppButton = () => {
 
   return (
     <>
-      {/* <Button link={alphadayApp} className={`bg-lightblue`}>
+      <Button link={alphadayApp} className={`bg-lightblue`}>
         Launch app
-      </Button> */}
-      <Button
-        disabled
-        className="mt-4 md:mt-0 bg-[#585858] hover:opacity-100 cursor-not-allowed self-start md:self-center whitespace-nowrap"
-      >
-        Coming soon
       </Button>
     </>
   );
