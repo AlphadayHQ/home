72b42140b475102e68bdeb560f629a477bd227d6
Felipe Faraggi <264382+faraggi@users.noreply.github.com>
2022-10-26 19:19:03 +0200

    v1.2.0 - emails, typos and rewording (#94)
    * [feat] Handle 404 errors in app. (#88)
    
    * handle 404
    
    * remove log
    
    * Update index.html
    
    remove capitalization from "Crypto" and "All"
    
    * Update Gasandoptions.jsx
    
    Change 1-click to One-click
    
    * Update WorkflowFrame.jsx
    
    Americanize the copy
    
    * Update Triplet.jsx
    
    Americanize the copy
    
    * Update Team.jsx
    
    Americanize the copy
    
    * Update Getfeatures.jsx
    
    Remove early alpha copy and change to stay up to date
    
    * Update faqData.jsx
    
    Lengthen/detail a FAQ
    
    * Update Hero.jsx
    
    change with (repetitive) to using and americanize the copy
    
    * [enhance] Update blog url in config (#91)
    
    * update blog config
    
    * route to blog in app
    
    * fix typo
    
    * use config
    
    * [fix] [ALPHA-1868] Send Emails to Alphaday (#90)
    
    * remove unecessary dependency
    
    * add url to config
    
    * create helper
    
    * send email to new endpoint
    
    * clean up unused styles
    
    * style fixes
    
    * Copy changes (#93)
    
    * Update Footer.jsx
    
    capitalize stuff in footer
    
    * Update Hero.jsx
    
    Make NFT plural, to follow the plural trend of L2s and DAOs
    
    Co-authored-by: Charles Nwankwo <39612820+Xavier-Charles@users.noreply.github.com>
    Co-authored-by: DeFiDude <59237470+DeFiDude@users.noreply.github.com>

--- src/config.js
+++ src/config.js
@@ -1,27 +1,28 @@
-import coverImg from "./images/seo1200x627-black.png"; 
+import coverImg from "./images/seo1200x627-black.png";
 
- const CONFIG = {
-   privacyPolicy:
-     "https://app.termly.io/document/privacy-policy/ae1c7443-6788-4493-91d3-ba6f87ff05e0",
-   terms:
-     "https://app.termly.io/document/terms-of-use-for-website/0f183823-fe52-47af-87d2-d1058b844918",
-   feedBack: "https://forms.gle/hBXWWTAvsN7dAMN66",
-   discord: "https://alphaday.com/discord",
-   twitter: "https://twitter.com/AlphadayHQ",
-   alphadayApp: "https://app.alphaday.com",
-   blog: "https://medium.com/@alphaday",
-   linkedin: "https://www.linkedin.com/company/alphaday",
-
-   seo: {
-     domain: "https://alphaday.com",
-     siteName: "Alphaday",
-     defaultTitle: "Alphaday",
-     defaultDescription: "Everything Crypto, All in one place.",
-     socialLinks: {
-       twitter: "https://twitter.com/AlphadayHQ",
-     },
-     cover: coverImg,
-   },
- };
+const CONFIG = {
+  privacyPolicy:
+    "https://app.termly.io/document/privacy-policy/ae1c7443-6788-4493-91d3-ba6f87ff05e0",
+  terms:
+    "https://app.termly.io/document/terms-of-use-for-website/0f183823-fe52-47af-87d2-d1058b844918",
+  feedBack: "https://forms.gle/hBXWWTAvsN7dAMN66",
+  discord: "https://alphaday.com/discord",
+  twitter: "https://twitter.com/AlphadayHQ",
+  alphadayApp: "https://app.alphaday.com",
+  blog: "https://alphaday.substack.com",
+  linkedin: "https://www.linkedin.com/company/alphaday",
+  emailSubscrptionUrl:
+    "https://api.zettaday.com/v1/user/email_subscriptions/subscribe/",
+  seo: {
+    domain: "https://alphaday.com",
+    siteName: "Alphaday",
+    defaultTitle: "Alphaday",
+    defaultDescription: "Everything Crypto, All in one place.",
+    socialLinks: {
+      twitter: "https://twitter.com/AlphadayHQ",
+    },
+    cover: coverImg,
+  },
+};
 
 export default CONFIG;
