a8deac3206dc42c79af56fd960a1c242cfdf9497
elcharitas <jonathanirhodia@gmail.com>
2023-01-09 12:56:29 +0100

    add subdomain urls

--- scripts/build-sitemap.js
+++ scripts/build-sitemap.js
@@ -2,11 +2,47 @@ const sitemaps = require("sitemaps");
 
 const filePath = `${__dirname}/../dist/sitemap.xml`;
 const baseUrl = "https://www.alphaday.com";
+const appUrl = "https://app.alphaday.com";
 
 const links = [
     {
         loc: baseUrl,
         priority: "1.00",
+        changefreq: "monthly",
+    },
+    {
+        loc: `${baseUrl}/blog`,
+        priority: "0.80",
+        changefreq: "weekly",
+    },
+    {
+        loc: appUrl,
+        priority: "1.00",
+        changefreq: "weekly",
+    },
+    {
+        loc: `${appUrl}/b/alpha`,
+        priority: "0.80",
+        changefreq: "weekly",
+    },
+    {
+        loc: `${appUrl}/b/beginner`,
+        priority: "0.80",
+        changefreq: "weekly",
+    },
+    {
+        loc: `${appUrl}/b/nft`,
+        priority: "0.80",
+        changefreq: "weekly",
+    },
+    {
+        loc: `${appUrl}/b/trading`,
+        priority: "0.80",
+        changefreq: "weekly",
+    },
+    {
+        loc: `${appUrl}/b/arbitrum`,
+        priority: "0.80",
         changefreq: "weekly",
     },
 ];
