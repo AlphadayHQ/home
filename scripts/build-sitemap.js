26f454195422357272c9dccd3a516a1ac854ad09
elcharitas <jonathanirhodia@gmail.com>
2023-01-09 12:26:23 +0100

    update sitemaps script

--- scripts/build-sitemap.js
+++ scripts/build-sitemap.js
@@ -9,31 +9,6 @@ const links = [
         priority: "1.00",
         changefreq: "weekly",
     },
-    {
-        loc: `${baseUrl}/blog`,
-        priority: "0.80",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${baseUrl}/b/alpha`,
-        priority: "0.80",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${baseUrl}/b/beginner`,
-        priority: "0.80",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${baseUrl}/b/nft`,
-        priority: "0.80",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${baseUrl}/b/trading`,
-        priority: "0.80",
-        changefreq: "weekly",
-    },
 ];
 
 sitemaps(filePath, links);
