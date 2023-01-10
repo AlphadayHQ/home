9922bffda4b1f014f9912b8e2bbdf36a4e1cc2fd
elcharitas <jonathanirhodia@gmail.com>
2023-01-10 09:07:01 +0100

    pr feedback

--- scripts/build-sitemap.js
+++ scripts/build-sitemap.js
@@ -1,7 +1,19 @@
 const sitemaps = require("sitemaps");
+const { resolve } = require("path");
+const { existsSync, mkdirSync } = require("fs");
 
-const filePath = `${__dirname}/../dist/sitemap.xml`;
-const baseUrl = "https://www.alphaday.com";
+// path to app build directory
+const distPath = resolve(__dirname, "../dist");
+const outputName = "sitemap.xml";
+const outputPath = `${distPath}/${outputName}`;
+
+// ensure the build directory exists
+// so we can run this script independent of the build.
+if (!existsSync(distPath)) {
+    mkdirSync(distPath);
+}
+
+const baseUrl = "https://alphaday.com";
 const appUrl = "https://app.alphaday.com";
 
 const links = [
@@ -22,34 +34,34 @@ const links = [
     },
     {
         loc: `${appUrl}/calendar`,
-        priority: "0.80",
+        priority: "0.60",
         changefreq: "weekly",
     },
     {
         loc: `${appUrl}/b/alpha`,
-        priority: "0.80",
+        priority: "0.70",
         changefreq: "weekly",
     },
     {
         loc: `${appUrl}/b/beginner`,
-        priority: "0.80",
+        priority: "0.60",
         changefreq: "weekly",
     },
     {
         loc: `${appUrl}/b/nft`,
-        priority: "0.80",
+        priority: "0.60",
         changefreq: "weekly",
     },
     {
         loc: `${appUrl}/b/trading`,
-        priority: "0.80",
+        priority: "0.60",
         changefreq: "weekly",
     },
     {
         loc: `${appUrl}/b/arbitrum`,
-        priority: "0.80",
+        priority: "0.60",
         changefreq: "weekly",
     },
 ];
 
-sitemaps(filePath, links);
+sitemaps(outputPath, links);
