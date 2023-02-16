671d72c406437a9be6f32cd1ed2d57f1d92f730e
Xharles <charlesnzubechukwu000@gmail.com>
2023-02-16 16:02:29 +0100

    update partners

--- src/components/home/partnersData.jsx
+++ src/components/home/partnersData.jsx
@@ -1,57 +1,109 @@
-import arbitrum from "../../images/partners/arbitrum.png";
+import aave from "../../images/partners/aave.png";
+import avalanche from "../../images/partners/avalanche.png";
+import aztec from "../../images/partners/aztec.png";
+import dfinity from "../../images/partners/dfinity.png";
+import impossible from "../../images/partners/impossible.png";
+import iotex from "../../images/partners/iotex.png";
+import moonbeam from "../../images/partners/moonbeam.png";
+import orbs from "../../images/partners/orbs.png";
+import rocketPool from "../../images/partners/rocketPool.png";
+import theGraph from "../../images/partners/theGraph.png";
+import zcash from "../../images/partners/zcash.png";
+
 import verasity from "../../images/partners/verasity.png";
 import sia from "../../images/partners/sia.png";
-import impossible from "../../images/partners/impossible.png";
-import bankless from "../../images/partners/bankless.png";
-import dfinity from "../../images/partners/dfinity.png";
 
 const partners = [
   {
     id: "1",
+    img: aave,
+    partner: "Aave",
+    slug: "aave",
+    size: "w-[120px]",
+  },
+  {
+    id: "2",
+    img: avalanche,
+    partner: "Avalanche",
+    slug: "avalanche",
+    size: "w-[50px]",
+  },
+  {
+    id: "3",
+    img: aztec,
+    partner: "Aztec",
+    slug: "aztec",
+    size: "w-[110px]",
+  },
+  {
+    id: "4",
+    img: dfinity,
+    partner: "Dfinity",
+    slug: "dfinity",
+    size: "w-[114px]",
+  },
+  {
+    id: "5",
+    img: impossible,
+    partner: "Impossible Finance",
+    slug: "impossible-finance",
+    size: "w-[100px]",
+  },
+  {
+    id: "6",
+    img: iotex,
+    partner: "Iotex",
+    slug: "iotex",
+    size: "w-[50px]",
+  },
+  {
+    id: "7",
+    img: moonbeam,
+    partner: "Moonbeam",
+    slug: "moonbeam",
+    size: "w-[140px]",
+  },
+  {
+    id: "8",
+    img: orbs,
+    partner: "Orbs",
+    slug: "orbs",
+    size: "w-[50px]",
+  },
+  {
+    id: "9",
+    img: rocketPool,
+    partner: "Rocket Pool",
+    slug: "rocket-pool",
+    size: "w-[52px]",
+  },
+  {
+    id: "10",
+    img: sia,
+    partner: "Sia",
+    slug: "sia",
+    size: "w-[50px]",
+  },
+  {
+    id: "11",
+    img: theGraph,
+    partner: "TheGraph",
+    slug: "the-graph",
+    size: "w-[50px]",
+  },
+  {
+    id: "12",
     img: verasity,
     partner: "Verasity",
     slug: "verasity",
     size: "w-[140px]",
   },
-
   {
-    id: "2",
-    partner: "Arbitrum",
-    img: arbitrum,
-    slug: "arbitrum",
+    id: "13",
+    img: zcash,
+    partner: "Zcash",
+    slug: "zcash",
     size: "w-[90px]",
-  },
-
-  {
-    id: "3",
-    partner: "Bankless",
-    img: bankless,
-    slug: "bankless",
-    size: "w-[114px]",
-  },
-
-  {
-    id: "4",
-    partner: "Impossible Finance",
-    img: impossible,
-    slug: "impossible",
-    size: "w-[100px]",
-  },
-
-  {
-    id: "5",
-    partner: "Sia",
-    img: sia,
-    slug: "sia",
-    size: "w-[45px]",
-  },
-
-  {
-    id: "6",
-    partner: "Dfinity",
-    img: dfinity,
-    slug: "dfinity",
-    size: "w-[114px]",
   },
 ];
 
