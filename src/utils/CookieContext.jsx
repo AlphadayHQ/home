1718eaf34dce9fc5d53ab0556798fb99056625ec
Xharles <charlesnzubechukwu000@gmail.com>
2023-03-11 13:46:23 +0100

    create cookie contsxt

---
+++ src/utils/CookieContext.jsx
@@ -0,0 +1,32 @@
+import { useState, createContext } from "react";
+
+export const CookieContext = createContext({
+  allowTracking: false,
+  noCookieChoice: true,
+  setCookieChoice: () => {},
+});
+
+const useCookieChoice = () => {
+  const storageCookieChoice = localStorage.getItem("cookieChoice");
+  const [cookieChoice, setCookieChoice] = useState(storageCookieChoice);
+
+  const handleCookieChioce = (choice) => {
+    setCookieChoice(choice);
+    localStorage.setItem("cookieChoice", choice);
+  };
+
+  return {
+    allowTracking: cookieChoice === "acceptAll",
+    noCookieChoice: cookieChoice === null,
+    setCookieChoice: handleCookieChioce,
+  };
+};
+
+export const CookieProvider = ({ children }) => {
+  const { allowTracking, setCookieChoice, noCookieChoice } = useCookieChoice();
+  return (
+    <CookieContext.Provider value={{ allowTracking, setCookieChoice, noCookieChoice }}>
+      {children}
+    </CookieContext.Provider>
+  );
+};
