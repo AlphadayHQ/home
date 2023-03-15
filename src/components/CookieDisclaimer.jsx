b1dc09b42b2f1fbc73b3f5c7bb5bb8ad24590f70
Xharles <charlesnzubechukwu000@gmail.com>
2023-03-15 16:08:01 +0100

    change button colorr

--- src/components/CookieDisclaimer.jsx
+++ src/components/CookieDisclaimer.jsx
@@ -29,8 +29,9 @@ const CookieDisclaimer = () => {
         <div className="relative flex flex-col flex-wrap justify-center items-center overflow-hidden">
           <div className="flex pt-5 p-2.5 lg:p-5 flex-1 flex-col lg:flex-row w-full max-w-7xl overflow-hidden items-center">
             <div className="mx-auto min-w-[280px] text-center lg:text-left text-[#C2C5D6] text-xs font-normal leading-6 tracking-wider">
-              We’d like to use other cookies to improve and personalize your visit and to analyze
-              our website’s performance, but only if you accept.
+              We’d like to use other cookies to improve and personalize your
+              visit and to analyze our website’s performance, but only if you
+              accept.
             </div>
             <div className="m-0 mt-2.5 lg:mt-0 flex flex-1 justify-end items-end scale-75">
               {choices
@@ -39,9 +40,10 @@ const CookieDisclaimer = () => {
                   <button
                     key={item.key}
                     onClick={item.handler}
-                    variant="primaryXL"
-                    className="outline-none inline-flex items-center justify-center tracking-wider text-center align-middle cursor-pointer leading-6 select-none transition-colors hover:outline-none active:outline-none focus:outline-none w-max-content h-34 px-10 lg:px-16 mx-2 py-4 min-w-max rounded-[10px] border-2 border-solid 
-                  border-[#263964] text-[#C2C5D6] bg-[#222529] hover:bg-[#2E3037] focus:bg-[#2E3037] active:border-2 active:border-solid active:border-[#263964] box-border duration-150 ease-in-out hover:border-[#477FF7] active:bg-[#17191C]"
+                    className={`${
+                      item.key === "decline" ? "bg-[#ff5f58]/[0.08]" : ""
+                    } outline-none inline-flex items-center justify-center tracking-wider text-center align-middle cursor-pointer leading-6 select-none transition-colors hover:outline-none active:outline-none focus:outline-none w-max-content h-34 px-10 lg:px-16 mx-2 py-4 min-w-max rounded-[10px] border-2 border-solid 
+                  border-[#263964] text-[#C2C5D6] bg-[#222529] hover:bg-[#2E3037] focus:bg-[#2E3037] active:border-2 active:border-solid active:border-[#263964] box-border duration-150 ease-in-out hover:border-[#477FF7] active:bg-[#17191C]`}
                   >
                     {item.buttonText}
                   </button>
