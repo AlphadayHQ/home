6f35df25856d036e3f3cc197f569fe050f229cae
Felipe Faraggi <264382+faraggi@users.noreply.github.com>
2023-04-25 10:04:38 +0200

    v1.2.8 - sitemap update (#120)
    * [feat] Update `build-sitemap` script to be dynamic
    
    Generate sitemap based on data from the backend
    
    * fix variable declaration
    
    ---------
    
    Co-authored-by: Gideon Anyalewechi <dev.giddy@gmail.com>

--- scripts/build-sitemap.js
+++ scripts/build-sitemap.js
@@ -10,81 +10,66 @@ const outputPath = `${distPath}/${outputName}`;
 // ensure the build directory exists
 // so we can run this script independent of the build.
 if (!existsSync(distPath)) {
-    mkdirSync(distPath);
+	mkdirSync(distPath);
 }
 
 const baseUrl = "https://alphaday.com";
 const appUrl = "https://app.alphaday.com";
 
-const links = [
-    {
-        loc: baseUrl,
-        priority: "1.00",
-        changefreq: "monthly",
-    },
-    {
-        loc: `${baseUrl}/blog`,
-        priority: "0.80",
-        changefreq: "weekly",
-    },
-    {
-        loc: appUrl,
-        priority: "1.00",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${appUrl}/calendar`,
-        priority: "0.40",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${appUrl}/b/alpha`,
-        priority: "0.70",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${appUrl}/b/beginner`,
-        priority: "0.60",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${appUrl}/b/nft`,
-        priority: "0.60",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${appUrl}/b/trading`,
-        priority: "0.60",
-        changefreq: "weekly",
-    },
+const boardsRequestUrl = "https://api.alphaday.com/ui/views/";
+
+// function to fetch all pages from the API looping through paginated results
+async function fetchBoards(url) {
+	const response = await fetch(url);
+	const { results, links } = await response.json();
+	let next = links.next;
+	let boards = [...results];
+	while (next) {
+		const response = await fetch(next);
+		const { results, links } = await response.json();
+		next = links.next;
+		boards = [...boards, ...results];
+	}
+	return boards;
+}
+
+const static_links = [
+	{
+		loc: baseUrl,
+		priority: "1.00",
+		changefreq: "monthly",
+	},
+	{
+		loc: `${baseUrl}/blog`,
+		priority: "0.80",
+		changefreq: "weekly",
+	},
+	{
+		loc: appUrl,
+		priority: "1.00",
+		changefreq: "weekly",
+	},
+	{
+		loc: `${appUrl}/calendar`,
+		priority: "0.40",
+		changefreq: "weekly",
+	},
 ];
 
-const partner_links = [
-    {
-        loc: `${appUrl}/b/arbitrum`,
-        priority: "0.60",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${appUrl}/b/bankless`,
-        priority: "0.60",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${appUrl}/b/ethereum`,
-        priority: "0.60",
-        changefreq: "weekly",
-    },
-    {
-        loc: `${appUrl}/b/verasity`,
-        priority: "0.60",
-        changefreq: "weekly",
-    },
-];
+// call fetchBoards to get all boards synchronously
+fetchBoards(boardsRequestUrl).then((boards) => {
+	const board_links = boards.map((board) => {
+		return {
+			loc: `${appUrl}/b/${board.slug}`,
+			priority: "0.60",
+			changefreq: "weekly",
+		};
+	});
 
-/**
- * Generate a sitemap.xml file for the app.
- *
- * Build the sitemap by adding the links and partner links to the sitemap
- */
-buildSitemap(outputPath, [...links, ...partner_links]);
+	/**
+	 * Generate a sitemap.xml file for the app.
+	 *
+	 * Build the sitemap by adding the links and partner links to the sitemap
+	 */
+	buildSitemap(outputPath, [...static_links, ...board_links]);
+});
