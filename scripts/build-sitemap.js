22baa9ff1c945247bc011ba04fbb7c0d5e290119
Charles Nwankwo <39612820+Xavier-Charles@users.noreply.github.com>
2023-05-09 16:15:05 +0100

    [fix] Handle Undefined Values in Sitemap  (#126)
    * add node-fetch
    
    * handle undefined values
    
    * test

--- scripts/build-sitemap.js
+++ scripts/build-sitemap.js
@@ -1,6 +1,7 @@
 const buildSitemap = require("sitemaps");
 const { resolve } = require("path");
 const { existsSync, mkdirSync } = require("fs");
+const fetch = require("node-fetch");
 
 // path to app build directory
 const distPath = resolve(__dirname, "../dist");
@@ -10,66 +11,65 @@ const outputPath = `${distPath}/${outputName}`;
 // ensure the build directory exists
 // so we can run this script independent of the build.
 if (!existsSync(distPath)) {
-	mkdirSync(distPath);
+  mkdirSync(distPath);
 }
 
 const baseUrl = "https://alphaday.com";
 const appUrl = "https://app.alphaday.com";
-
 const boardsRequestUrl = "https://api.alphaday.com/ui/views/";
 
 // function to fetch all pages from the API looping through paginated results
 async function fetchBoards(url) {
-	const response = await fetch(url);
-	const { results, links } = await response.json();
-	let next = links.next;
-	let boards = [...results];
-	while (next) {
-		const response = await fetch(next);
-		const { results, links } = await response.json();
-		next = links.next;
-		boards = [...boards, ...results];
-	}
-	return boards;
+  const response = await fetch(url);
+  const { results, links } = await response.json();
+  let next = links?.next;
+  let boards = [...(results || [])];
+  while (next) {
+    const response = await fetch(next);
+    const { results, links } = await response.json();
+    next = links?.next;
+    boards = [...boards, ...(results || [])];
+  }
+  return boards;
 }
 
 const static_links = [
-	{
-		loc: baseUrl,
-		priority: "1.00",
-		changefreq: "monthly",
-	},
-	{
-		loc: `${baseUrl}/blog`,
-		priority: "0.80",
-		changefreq: "weekly",
-	},
-	{
-		loc: appUrl,
-		priority: "1.00",
-		changefreq: "weekly",
-	},
-	{
-		loc: `${appUrl}/calendar`,
-		priority: "0.40",
-		changefreq: "weekly",
-	},
+  {
+    loc: baseUrl,
+    priority: "1.00",
+    changefreq: "monthly",
+  },
+  {
+    loc: `${baseUrl}/blog`,
+    priority: "0.80",
+    changefreq: "weekly",
+  },
+  {
+    loc: appUrl,
+    priority: "1.00",
+    changefreq: "weekly",
+  },
+  {
+    loc: `${appUrl}/calendar`,
+    priority: "0.40",
+    changefreq: "weekly",
+  },
 ];
 
 // call fetchBoards to get all boards synchronously
 fetchBoards(boardsRequestUrl).then((boards) => {
-	const board_links = boards.map((board) => {
-		return {
-			loc: `${appUrl}/b/${board.slug}`,
-			priority: "0.60",
-			changefreq: "weekly",
-		};
-	});
+  const board_links = boards.map((board) => {
+    return {
+      loc: `${appUrl}/b/${board.slug}`,
+      priority: "0.60",
+      changefreq: "weekly",
+    };
+  });
 
-	/**
-	 * Generate a sitemap.xml file for the app.
-	 *
-	 * Build the sitemap by adding the links and partner links to the sitemap
-	 */
-	buildSitemap(outputPath, [...static_links, ...board_links]);
+  /**
+   * Generate a sitemap.xml file for the app.
+   *
+   * Build the sitemap by adding the links and partner links to the sitemap
+   */
+  buildSitemap(outputPath, [...static_links, ...board_links]);
 });
