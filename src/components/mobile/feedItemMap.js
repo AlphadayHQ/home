e6542024cba201df20aadb36477404484b418ac7
Xharles <charlesnzubechukwu000@gmail.com>
2024-03-12 18:21:38 +0100

    add icon map

---
+++ src/components/mobile/feedItemMap.js
@@ -0,0 +1,29 @@
+import blogIcon from "../../assets/svg/feed-icons/blog.svg";
+import eventIcon from "../../assets/svg/feed-icons/calendar.svg";
+import forumIcon from "../../assets/svg/feed-icons/forum.svg";
+import imageIcon from "../../assets/svg/feed-icons/image.svg";
+import newsIcon from "../../assets/svg/feed-icons/news.svg";
+import personIcon from "../../assets/svg/feed-icons/star.svg";
+import podcastIcon from "../../assets/svg/feed-icons/podcast.svg";
+import socialIcon from "../../assets/svg/feed-icons/rss.svg";
+import priceActionIcon from "../../assets/svg/feed-icons/price-action.svg";
+import memeIcon from "../../assets/svg/feed-icons/memes.svg";
+import tvlIcon from "../../assets/svg/feed-icons/TVL.svg";
+import videoIcon from "../../assets/svg/feed-icons/play.svg";
+
+export const feedItemIconMap = {
+  news: newsIcon,
+  event: eventIcon,
+  video: videoIcon,
+  podcast: podcastIcon,
+  blog: blogIcon,
+  forum: forumIcon,
+  person: personIcon,
+  image: imageIcon,
+  meme: imageIcon,
+  reddit: socialIcon,
+  DISCORD: socialIcon,
+  market: priceActionIcon,
+  tvl: tvlIcon,
+  meme: memeIcon,
+};
