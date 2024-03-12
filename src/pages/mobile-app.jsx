e789181201a5867d28994d9b938d6dcc8b2220da
Xharles <charlesnzubechukwu000@gmail.com>
2024-03-12 18:22:09 +0100

    add various sections with info component

--- src/pages/mobile-app.jsx
+++ src/pages/mobile-app.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Footer, Navbar } from "../components";
 import Hero from "../components/mobile/Hero";
 import Video from "../components/mobile/Video";
-import { Backers, Partners } from "../components/home";
+import { Backers, Partners, Workflow } from "../components/home";
+import Info from "../components/mobile/Info";
+import onTheGoImage from "../images/mobile/on-the-go.png";
+import SwipeUpImage from "../images/mobile/swipe-up.png";
+import NotificationsImage from "../images/mobile/notifications.png";
+import PillsImage from "../images/mobile/pills-image.png";
+import Stats from "../components/home/Stats";
 
 const MobilePage = () => {
   return (
@@ -11,12 +17,41 @@ const MobilePage = () => {
       <Hero />
       <Video />
       <Backers />
+      <Info
+        emoji="🏃"
+        title="Crypto on the go"
+        text="Experience the ultimate convenience with Alphaday and fit your entire crypto workflow right in your pocket."
+        img={onTheGoImage}
+      />
+      <Info
+        emoji="👆"
+        title="Swipe up for alpha"
+        text="Alphaday is here to change the way you interact with your crypto feed completely."
+        img={SwipeUpImage}
+        bg="bg-eerie"
+      />
+      <div className="mx-auto w-11/12 max-w-7xl">
+        <Stats />
+      </div>
+      <Info
+        emoji="🚀"
+        title="Unlock the Superfeed"
+        text="Forget opening dozens of tabs, Superfeed is your personalized crypto news aggregator."
+        img={PillsImage}
+        bg="bg-eerie"
+      />
+      <Info
+        emoji="🔔"
+        title="Never miss a beat"
+        text="Stay ahead with instant alerts on market trends, coin updates, and tailored notifications."
+        img={NotificationsImage}
+        bg="bg-eerie"
+      />
       {/* <Workflow />
       <Partners />
       <Backers />
       <Team />
       <FAQ />
-      <Getfeatures />
       <Footer /> */}
       <div className="mb-40"></div>
       {/* This is a hack to fix the footer position */}
