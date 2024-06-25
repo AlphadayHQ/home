27bc01d3264539c4934e996a8afb4a69f77c3d65
Felipe Faraggi <264382+faraggi@users.noreply.github.com>
2024-06-25 11:23:09 +0200

    chore: updated partners (#176)
    * chore: updated partners
    
    * chore: updated again
    
    * remove unsed items
    
    * increase worldcoin logo size
    
    ---------
    
    Co-authored-by: Xharles <charlesnzubechukwu000@gmail.com>

--- src/components/home/partnersData.jsx
+++ src/components/home/partnersData.jsx
@@ -1,11 +1,8 @@
 import aave from "../../images/partners/aave.png";
 import avalanche from "../../images/partners/avalanche.png";
-import aztec from "../../images/partners/aztec.png";
 import dfinity from "../../images/partners/dfinity.png";
 import impossible from "../../images/partners/impossible.png";
 import iotex from "../../images/partners/iotex.png";
-import moonbeam from "../../images/partners/moonbeam.png";
-import orbs from "../../images/partners/orbs.png";
 import rocketPool from "../../images/partners/rocketPool.png";
 import theGraph from "../../images/partners/theGraph.png";
 import zcash from "../../images/partners/zcash.png";
@@ -14,6 +11,11 @@ import sia from "../../images/partners/sia.png";
 import chiliz from "../../images/partners/chiliz.png";
 import ethereumClassic from "../../images/partners/ethereum-classic.png";
 import oceanProtocol from "../../images/partners/ocean-protocol.png";
+import polygon from "../../images/partners/polygon.png";
+import sui from "../../images/partners/sui.png";
+import worldcoin from "../../images/partners/worldcoin.png";
+import ripple from "../../images/partners/ripple.png";
+import metis from "../../images/partners/metis.png";
 
 const partners = [
   {
@@ -32,10 +34,10 @@ const partners = [
   },
   {
     id: "3",
-    img: aztec,
-    partner: "Aztec",
-    slug: "aztec",
-    size: "w-[90px]",
+    img: polygon,
+    partner: "Polygon",
+    slug: "polygon",
+    size: "w-[120px]",
   },
   {
     id: "4",
@@ -56,77 +58,91 @@ const partners = [
     img: iotex,
     partner: "Iotex",
     slug: "iotex",
-    size: "w-[120px]",
-  },
-  {
-    id: "7",
-    img: moonbeam,
-    partner: "Moonbeam",
-    slug: "moonbeam",
     size: "w-[140px]",
   },
   {
-    id: "8",
-    img: orbs,
-    partner: "Orbs",
-    slug: "orbs",
-    size: "w-[80px]",
-  },
-  {
-    id: "9",
+    id: "7",
     img: rocketPool,
     partner: "Rocket Pool",
     slug: "rocketpool",
     size: "w-[150px]",
   },
   {
-    id: "10",
+    id: "8",
     img: sia,
     partner: "Sia",
     slug: "sia",
     size: "w-[120px]",
   },
   {
-    id: "11",
+    id: "9",
     img: theGraph,
     partner: "TheGraph",
     slug: "thegraph",
     size: "w-[140px]",
   },
   {
-    id: "12",
-    img: verasity,
-    partner: "Verasity",
-    slug: "verasity",
-    size: "w-[140px]",
-  },
-  {
-    id: "13",
+    id: "10",
     img: zcash,
     partner: "Zcash",
     slug: "zcash",
     size: "w-[90px]",
   },
   {
-    id: "14",
+    id: "11",
     img: chiliz,
     partner: "Chiliz",
     slug: "chiliz",
     size: "w-[90px]",
   },
   {
-    id: "15",
+    id: "12",
     img: ethereumClassic,
     partner: "Ethereum Classic",
     slug: "ethereumclassic",
-    size: "w-[140px]",
+    size: "w-[90px]",
   },
   {
-    id: "16",
+    id: "13",
     img: oceanProtocol,
     partner: "Ocean Protocol",
     slug: "oceanprotocol",
     size: "w-[70px]",
+  },
+  {
+    id: "14",
+    img: verasity,
+    partner: "Verasity",
+    slug: "verasity",
+    size: "w-[90px]",
+  },
+  {
+    id: "15",
+    img: sui,
+    partner: "Sui",
+    slug: "sui",
+    size: "w-[80px]",
+  },
+  {
+    id: "16",
+    img: worldcoin,
+    partner: "Worldcoin",
+    slug: "worldcoin",
+    size: "w-[150px]",
+  },
+  {
+    id: "17",
+    img: ripple,
+    partner: "Ripple",
+    slug: "ripple",
+    size: "w-[90px]",
+  },
+  {
+    id: "18",
+    img: metis,
+    partner: "Metis",
+    slug: "metis",
+    size: "w-[120px]",
   },
 ];
 
